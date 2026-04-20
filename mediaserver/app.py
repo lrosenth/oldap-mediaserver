d321a8fe41fedb00aea4d64f1ce70b7a8fff635c
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2026-04-20 21:06:09 +0200

    Bug fix for caching

--- mediaserver/app.py
+++ mediaserver/app.py
@@ -217,7 +217,7 @@ def create_app() -> Flask:
 
     cors_origins = env_list(
         "CORS_ORIGINS",
-        "http://localhost:5173,http://127.0.0.1:5173"
+        "http://localhost:5173,http://127.0.0.1:5173,http://localhost:5174,http://127.0.0.1:5174"
     )
 
     CORS(
